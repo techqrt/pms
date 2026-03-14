29d5b1505a38ec0cd2954724ff581e3af9c48f3b
Rayyan Shaikh <rayyanshaikh@192.168.1.6>
2026-03-14 05:13:48 -0400

    Updated token Lifetime

--- pms/settings.py
+++ pms/settings.py
@@ -179,8 +179,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
